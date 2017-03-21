216d0422dab82b6a668e252ffb979b1a33de715f
rishikant <rksbtp@gmail.com>
2017-03-21 22:57:51 +0530

    added quiz1 soln

--- ch1/quiz1c.py
+++ ch1/quiz1c.py
@@ -9,6 +9,7 @@ enrollments = read_csv('../data/enrollments.csv')
 daily_engagement = read_csv('../data/daily_engagement.csv')
 project_submissions = read_csv('../data/project_submissions.csv')
         
+print len(enrollments)
 print enrollments[0] 
 print daily_engagement[0]
 print project_submissions[0]
